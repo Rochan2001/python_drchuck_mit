3b1c5617366cd9561fbfb17280c43f0eb7413e25
B.ROCHAN SINGH <bahadur.rochan@gmail.com>
2020-09-05 17:09:53 +0530

    first commit

---
+++ hidden.py
@@ -0,0 +1,10 @@
+# Keep this file separate
+
+# https://apps.twitter.com/
+# Create new App and get the four strings
+
+def oauth():
+    return {"consumer_key": "FAgrqf6laSMowoIE5MsNwSlTh",
+            "consumer_secret": "ym7ptAVU2EhoiwHvzdhrWKEzMKD72rMjuofJiyK6MWFyyvVKFx",
+            "token_key": "1274955779917754368-PiERS3wVSIGgiqRxdPTeO2HH0k2KwM",
+            "token_secret": "dFJQ83XfIyOI8kX6uCPgJYV9jIHMe2f7v2wqBEzjO9hpL"}
